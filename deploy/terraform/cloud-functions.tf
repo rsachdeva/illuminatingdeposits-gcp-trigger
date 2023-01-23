be8645f21c50175c579713701d6d17fea9f4fca8
Rohit Sachdeva <growth@drinnovations.us>
2023-01-23 12:47:22 -0600

    some terrafrom related infrastructure to get started

---
+++ deploy/terraform/cloud-functions.tf
@@ -0,0 +1,24 @@
+resource "google_storage_bucket" "illuminating_bucket" {
+  name = "illuminating_gcp_trigger"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "illuminating_src_code" {
+  name = "gomain.zip"
+  bucket = google_storage_bucket.illuminating_bucket.name
+  source = "gomain.zip"
+}
+
+resource "google_cloudfunctions_function" "illuminating_deposits_func" {
+  name = "fun-from-tf"
+  runtime = "go119"
+  description = "function from terraform script using go 1.19"
+
+  available_memory_mb = 128
+  source_archive_bucket = google_storage_bucket.illuminating_bucket.name
+  source_archive_object = google_storage_bucket_object.illuminating_src_code.name
+
+  trigger_http = true
+  entry_point = "HelloWorld"
+
+}
